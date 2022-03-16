1ec8b8190eb63d024ee6c5d3b61c8147b0fd9233
Esteban Pinto L <estebanpinto1284@gmail.com>
2022-03-16 10:51:20 -0700

    Final produt 2_REAL FINAL PRODUCT It is better to mantain the origina python script and its name otherwise I cannot see the changes that I did 3 linear regression, one per Iris species

--- regress_plot.py
+++ regress_plot.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python3
 
 
-"Exercise5: Generate a script to perform linear regressions between structural flower whorls (petals vs  sepals) of three Iris species,& to graph this relationship using a scatterplot"
+"Exercise5: Generate a script to perform linear regressions between structural flower whorls (petals vs  sepals) of three Iris species"
 
 
 import pandas as pd
@@ -25,32 +25,69 @@ def iris_scatterplot():
     Requierements
     ---------
     CSV file that contains the flower information of three Iris species ("iris.csv")
-    
+
+    First step is to create new variables (objects) that contain the flower whorls information per each species
+    We will get 3 new variables (objects)
+
     """
 
     dataframe = pd.read_csv("iris.csv")
     type(dataframe)
+
     setosa = dataframe[dataframe.species == "Iris_setosa"]
     versicolor = dataframe[dataframe.species == "Iris_versicolor"]
     virginica = dataframe[dataframe.species == "Iris_virginica"]
 
+    """
+    Once that the new variables where created, this script is going to cretea 3 scatterplots, one per scpecies
+
+    """
 
     x = setosa.petal_length_cm
     y = setosa.sepal_length_cm
     regression = stats.linregress(x, y)
     slope = regression.slope
     intercept = regression.intercept
-    plt.scatter(x, y, label = 'Iris setosa whrols flower length regression')
+    plt.scatter(x, y, label = 'I.setosa whrols flower length regression')
     plt.plot(x, slope * x + intercept, color = "red", label = 'Fitted line')
     plt.xlabel("Petal length (cm)")
     plt.ylabel("Sepal length (cm)")
     plt.legend()
     plt.savefig("Iris_setosa_regression.png")
+    plt.clf()
+    print("You got 1 scatterplot")
 
 
+    x = versicolor.petal_length_cm
+    y = versicolor.sepal_length_cm
+    regression = stats.linregress(x, y)
+    slope = regression.slope
+    intercept = regression.intercept
+    plt.scatter(x, y, label = 'I.versicolor whrols flower length regression')
+    plt.plot(x, slope * x + intercept, color = "green", label = 'Fitted line')
+    plt.xlabel("Petal length (cm)")
+    plt.ylabel("Sepal length (cm)")
+    plt.legend()
+    plt.savefig("Iris_versicolor_regression.png")
+    plt.clf()
+    print("You got 2 scatterplots")
+
+
+    x = virginica.petal_length_cm
+    y = virginica.sepal_length_cm
+    regression = stats.linregress(x, y)
+    slope = regression.slope
+    intercept = regression.intercept
+    plt.scatter(x, y, label = 'I.virginica whrols flower length regression')
+    plt.plot(x, slope * x + intercept, color = "orange", label = 'Fitted line')
+    plt.xlabel("Petal length (cm)")
+    plt.ylabel("Sepal length (cm)")
+    plt.legend()
+    plt.savefig("Iris_virginica_regression.png")
+    plt.clf()
     print("You got 3 scatterplots, one per Iris species")
 
 
-    if __name__ == '__main__':
-         iris_scatterplot()
+if __name__ == '__main__':
+    iris_scatterplot()
 
